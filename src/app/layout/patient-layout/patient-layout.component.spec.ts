ebea9f641100daa917fc3d03b769db1b8299ffe5
Kamalesh <rajankamalesh92@gmail.com>
2023-09-14 18:54:54 +0530

    Ui changes

--- src/app/layout/patient-layout/patient-layout.component.spec.ts
+++ src/app/layout/patient-layout/patient-layout.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PatientLayoutComponent } from './patient-layout.component';
 
 describe('PatientLayoutComponent', () => {
@@ -8,9 +7,8 @@ describe('PatientLayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PatientLayoutComponent ]
-    })
-    .compileComponents();
+      declarations: [PatientLayoutComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PatientLayoutComponent);
     component = fixture.componentInstance;
@@ -20,4 +18,12 @@ describe('PatientLayoutComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have initial state values', () => {
+    expect(component.recordingState).toBe('Start Recording');
+    expect(component.showTextArea).toBe(false);
+    expect(component.showSubmitButton).toBe(false);
+    expect(component.showThankYou).toBe(false);
+  });
 });
+
--- src/app/layout/patient-layout/patient-layout.component.ts
+++ src/app/layout/patient-layout/patient-layout.component.ts
@@ -1,26 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { VoiceRecognitionService } from 'src/app/shared/service/voice-recognition.service';
+// import { ApiService } from 'src/app/api.service'; // Adjust the path to match your project structure
+ // Import the ApiService
 
 @Component({
   selector: 'app-patient-layout',
   templateUrl: './patient-layout.component.html',
   styleUrls: ['./patient-layout.component.scss'],
-  providers: [VoiceRecognitionService]
+  providers: [VoiceRecognitionService],
 })
-export class PatientLayoutComponent implements OnInit {
-  text: String | undefined;
+export class PatientLayoutComponent implements OnDestroy {
+  recordingState = 'Start Recording';
+  showTextArea = false;
+  showSubmitButton = false;
+  showThankYou = false;
+
   constructor(
-    public service: VoiceRecognitionService
-  ) { 
-    this.service.init()
+    public service: VoiceRecognitionService,
+    // private apiService: ApiService // Inject the ApiService
+  ) {
+    this.service.init();
   }
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    // Ensure the speech recognition service is stopped when the component is destroyed
+    this.service.stop();
   }
-  start(){
-    this.service.start()
+
+  toggleRecording() {
+    if (this.recordingState === 'Start Recording' || this.recordingState === 'Recording Stopped') {
+      // Start recording
+      this.recordingState = 'Recording';
+      this.showTextArea = true;
+      this.showSubmitButton = true;
+      this.service.start();
+    } else if (this.recordingState === 'Recording') {
+      // Stop recording
+      this.recordingState = 'Recording Stopped';
+      this.service.stop();
+    }
   }
-  stop(){
-    this.service.stop()
+
+  submitResponse() {
+    if (this.recordingState === 'Recording') {
+      // Alert user to stop recording first before submitting
+      alert('Stop recording first before submitting.');
+    } else {
+      // Send the recorded text to the backend API
+      this.service.stop(); // Ensure the speech recognition service is stopped
+      const recordedText = this.service.text;
+
+      if (recordedText) {
+        // Send the recorded text to the API
+        // this.apiService.sendRecordedText({ recorded_text: recordedText }).subscribe(
+        //   (response) => {
+        //     console.log('API Response:', response);
+        //     this.showTextArea = false;
+        //     this.showSubmitButton = false;
+        //     this.showThankYou = true;
+        //   },
+        //   (error) => {
+        //     console.error('API Error:', error);
+        //     // Handle API error here
+        //   }
+        // );
+      } else {
+        alert('No recorded text to submit.');
+      }
+    }
   }
 }
